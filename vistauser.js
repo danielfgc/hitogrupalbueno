5a1ff95ff178b3642a981c3653d64df13702b8d9
Daniel Fernandez <daniel.fernandez@campusfp.es>
2022-03-30 14:37:36 +0200

    hola hola

--- vistauser.js
+++ vistauser.js
@@ -1,6 +1,99 @@
-function pintarCabecera(){
-    var hs = document.getElementById('hs').value;
-    var hcont = document.getElementById('hcont').value;
 
-    document.getElementById('cabecera').innerHTML = "<"+hs+">"+hcont+"</"+hs+">";
+var contadorh = 0;
+var contadorp = 0;
+
+function clicar(event){
+          
+        
+            
+    var n = event.currentTarget.id;
+    sessionStorage.setItem("galleta",n);
+
+    
+}
+
+
+
+function pintarCabecera(){
+    
+        var ide =sessionStorage.getItem("galleta");
+        var hs = document.getElementById('hs'+ide).value;
+        var hcont = document.getElementById('hcont'+ide).value;
+        document.getElementById('div'+ide).innerHTML = "<"+hs+">"+hcont+"</"+hs+">";
+    
+}
+
+
+
+
+function eliminarElementoH(){
+    var ide =sessionStorage.getItem("galleta");
+    
+    var padre = document.getElementById('cabecera');
+    var hijo = document.getElementById('div'+ide);
+    var padre2 = document.getElementById('pintar');
+    var hijo2 = document.getElementById(ide);
+
+    padre.removeChild(hijo);
+    padre2.removeChild(hijo2);
+}
+
+function agregarTitulo(){
+    contadorh++;
+         var pintar = document.getElementById('pintar');
+         var lista = document.createElement('li'); 
+         
+        lista.innerHTML = '    <select name="hs" id="hs'+contadorh+'" onchange="pintarCabecera()">\
+        <option value="h1">h1</option>\
+        <option value="h2">h2</option>\
+        <option value="h3">h3</option>\
+        <option value="h4">h4</option>\
+        <option value="h5">h5</option>\
+        </select>\
+        <input type="text" name="hcont" id="hcont'+contadorh+'" onkeyup="pintarCabecera()">\
+        <button id="eh'+contadorh+'" onclick="eliminarElementoH()">Eliminar</button>';
+        pintar.appendChild(lista);
+        var div= document.createElement('div');
+        div.setAttribute( 'id', "div"+contadorh);
+        lista.setAttribute( 'id', ""+contadorh);
+        document.getElementById('cabecera').appendChild(div);   
+        lista.addEventListener("mouseover", function(event){clicar(event)});  
+    
+}
+
+function pintarParrafo(){
+    
+    var ide =sessionStorage.getItem("galleta");
+    var ps = document.getElementById('ps'+ide).value;
+    var pcont = document.getElementById('pcont'+ide).value;
+    document.getElementById('divp'+ide).innerHTML = "<"+ps+">"+pcont+"</"+ps+">";
+
+}
+
+function eliminarElementoP(){
+    var ide =sessionStorage.getItem("galleta");
+    var padre = document.getElementById('cabecera');
+    var hijo = document.getElementById('divp'+ide);
+    var padre2 = document.getElementById('parrafos');
+    var hijo2 = document.getElementById(ide);
+    padre2.removeChild(hijo2);
+    padre.removeChild(hijo);
+}
+function agregarParrafo(){
+    contadorp++;
+         var parrafos = document.getElementById('parrafos');
+         var list = document.createElement('li'); 
+         
+        list.innerHTML = '    <select name="hs" id="ps'+contadorp+'" onchange="pintarParrafo()">\
+        <option value="p">p</option>\
+        </select>\
+        <input type="text" name="hcont" id="pcont'+contadorp+'" onkeyup="pintarParrafo()">\
+        <button id="eh'+contadorp+'" onclick="eliminarElementoP()">Eliminar</button>';
+        parrafos.appendChild(list);
+        var div= document.createElement('div');
+        div.setAttribute( 'id', "divp"+contadorp);
+        list.setAttribute( 'id', contadorp);
+        document.getElementById('cabecera').appendChild(div);   
+        list.addEventListener("mouseover", function(event){clicar(event)}); 
+          
 }
--- vistauser.php
+++ vistauser.php
@@ -1,17 +1,27 @@
 
 
-<form action="actualizar vista" method="post" onchange="pintarCabecera()">
-    <select name="hs" id="hs">
-        <option value="h1">h1</option>
-        <option value="h2">h2</option>
-        <option value="h3">h3</option>
-        <option value="h4">h4</option>
-        <option value="h5">h5</option>
-    </select>
-    <input type="text" name="hcont" id="hcont" onkeyup="pintarCabecera()">
-</form>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<header class="container d-flex justify-content-center" id="cabecera">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <aside class="fixed-left col-md-4">
+    <div id="pintar">
+        
+<button id="boton" onclick="agregarTitulo()">Generar Titulo</button>
+</div>
+<div id="parrafos"><button onclick="agregarParrafo()">Generar parrafo</button></div>
+</aside>
+<header class="fixed-right container col-md-8" id="cabecera">
 
 </header>
 
@@ -24,3 +34,6 @@
 
 </footer>
 <script src="vistauser.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
